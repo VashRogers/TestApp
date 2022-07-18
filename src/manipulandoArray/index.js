12fae3cec91ed16f5e06779ed60cc6187f9a6752
VashRogers <rogerssj2@gmail.com>
2022-07-18 15:33:34 -0400

    Editando arrays mocados

--- src/manipulandoArray/index.js
+++ src/manipulandoArray/index.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState, useCallback, useRef } from 'react';
+import React, { useEffect, useState, useCallback, useRef, useMemo } from 'react';
 import { Text, View, FlatList, StyleSheet, TouchableOpacity, RefreshControl, ActivityIndicator } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import axios from 'axios';
 
-const response = [
+//response == Schedule
+const response = [ 
     {
         id: 1,
         nome: "Semanal",
@@ -43,60 +44,179 @@ const response = [
         ]
     }
 ]
-
-const responseTeste = [
+//responseTeste == get-employees-punches
+ const responseTeste = [
     {
-        id: 0,
-        texto:'Teste de Draggable',
-        desc:'Descrição curta pra caceta',
-        background:'red'
+        "id": 5,
+        "funcionario_id": 1,
+        "horario_id": 1,
+        "hora": "08:30:00",
+        "data": "2022-07-18",
+        "dispositivo": "Operational System: android, Version: 32",
+        "localizacao": "{\"latitude\": -22.2239, \"longitude\": -54.8057667 }",
+        "justificativa": null
     },
-    
     {
-        id: 1,
-        texto:'Teste de Draggable2',
-        desc:'Descrição longa lorem ipsum sit dolor amet, dolor, lit, zapdos, pokemon radpidfire. Je sui Madmouseille, tomato juice; song of madness',
-        background:'blue'
+        "id": 6,
+        "funcionario_id": 1,
+        "horario_id": 1,
+        "hora": "08:30:00",
+        "data": "2022-07-18",
+        "dispositivo": "Operational System: android, Version: 32",
+        "localizacao": "{\"latitude\": -22.2239, \"longitude\": -54.8057667 }",
+        "justificativa": null
     },
-    
     {
-        id: 2,
-        texto:'TerraFirmação',
-        desc:'Terra transform the earth on the table in the react native app',
-        background:'green'
+        "id": 7,
+        "funcionario_id": 1,
+        "horario_id": 1,
+        "hora": "08:30:00",
+        "data": "2022-07-18",
+        "dispositivo": "Operational System: android, Version: 32",
+        "localizacao": "{\"latitude\": -22.2239, \"longitude\": -54.8057667 }",
+        "justificativa": null
     },
-   
-    
+    {
+        "id": 8,
+        "funcionario_id": 1,
+        "horario_id": 1,
+        "hora": "08:50:00",
+        "data": "2022-07-18",
+        "dispositivo": "Operational System: android, Version: 32",
+        "localizacao": "{\"latitude\": -22.2239, \"longitude\": -54.8057667 }",
+        "justificativa": null
+    },
+    {
+        "id": 9,
+        "funcionario_id": 1,
+        "horario_id": 1,
+        "hora": "10:41:00",
+        "data": "2022-07-18",
+        "dispositivo": "Operational System: android, Version: 32",
+        "localizacao": "{\"latitude\": -22.2239, \"longitude\": -54.8057667 }",
+        "justificativa": null
+    },
+    {
+        "id": 10,
+        "funcionario_id": 1,
+        "horario_id": 1,
+        "hora": "11:01:00",
+        "data": "2022-07-18",
+        "dispositivo": "Operational System: android, Version: 32",
+        "localizacao": "{\"latitude\": -22.2239, \"longitude\": -54.8057667 }",
+        "justificativa": null
+    },
+    {
+        "id": 11,
+        "funcionario_id": 1,
+        "horario_id": 1,
+        "hora": "11:22:00",
+        "data": "2022-07-18",
+        "dispositivo": "Operational System: android, Version: 32",
+        "localizacao": null,
+        "justificativa": null
+    },
+    {
+        "id": 12,
+        "funcionario_id": 1,
+        "horario_id": 1,
+        "hora": "13:06:00",
+        "data": "2022-07-18",
+        "dispositivo": "Operational System: android, Version: 32",
+        "localizacao": "{\"latitude\": -22.2239, \"longitude\": -54.8057667 }",
+        "justificativa": null
+    },
+    {
+        "id": 13,
+        "funcionario_id": 1,
+        "horario_id": 1,
+        "hora": "14:05:00",
+        "data": "2022-07-18",
+        "dispositivo": "Operational System: android, Version: 32",
+        "localizacao": "{\"latitude\": -22.2239, \"longitude\": -54.8057667 }",
+        "justificativa": null
+    }
 ]
+
+//[ 
+//     {
+//         id: 0,
+//         texto:'Teste de Draggable',
+//         desc:'Descrição curta pra caceta',
+//         background:'red'
+//     },
+    
+//     {
+//         id: 1,
+//         texto:'Teste de Draggable2',
+//         desc:'Descrição longa lorem ipsum sit dolor amet, dolor, lit, zapdos, pokemon radpidfire. Je sui Madmouseille, tomato juice; song of madness',
+//         background:'blue'
+//     },
+    
+//     {
+//         id: 2,
+//         texto:'TerraFirmação',
+//         desc:'Terra transform the earth on the table in the react native app',
+//         background:'green'
+//     },
+
+//     {
+//         id: 3,
+//         texto:'TerraFirmação',
+//         desc:'Terra transform the earth on the table in the react native apsadasasdasdap',
+//         background:'green'
+//     },
+//     // {
+//     //     id: 4,
+//     //     texto:'TerraFirmação',
+//     //     desc:'Terra transform the earth on the table in the react native ROGER TESTE TESTE',
+//     //     background:'green'
+//     // },
+    
+// ]
 
 export default function ManipulandoArray (){
 
-    const [ mainData, setMainData ] = useState(response)
-    const [ data, setData ] = useState(responseTeste)
+    const [ schedule, setSchedule ] = useState(response)
+    const [ punches, setPunches ] = useState(responseTeste)
     const [newDateTeste, setNewDateTeste] = useState([])
 
-    const [ auxState, setAuxState ] = useState(mainData[0].hours)
+    // const [ auxState, setAuxState ] = useState(mainData[0].hours)
 
     
 
-    const createViewItemsDrag = useCallback(() => {
-        const data = []
-        response[0]?.hours.forEach((element, otherKey) => {
-           const isElement = responseTeste.find((_, key) => key === element.ordem) || false
-          
-           if(isElement){
-             return data.push(isElement)
+    const arrayOfOrganizedPoints = useMemo(() => {
+        const arr = []
+
+        const { hours: fieldHoursArray }= response[0];
+
+        const punchesArray = responseTeste;
+
+        fieldHoursArray.forEach((element, otherKey) => {
+
+           const thereIsPoint = punchesArray.find((_, key) => key === element.ordem);
+
+           if(thereIsPoint){
+
+            return arr.push(thereIsPoint);
+
            } 
+
             
-        //    data.push({ id: otherKey, texto: null , desc: null,background: null })
-           return setNewDateTeste(data)
-        })
+            // return console.log('aqui seria a logica para adicionar o campo vazio que falta preencher'); 
+            return arr.push({teste: '', id: otherKey , outros: ''});        
 
+          })
+
+        return arr
+
+    }, [response, responseTeste])
+
+
+
+    useEffect(() => {       
+        setNewDateTeste(arrayOfOrganizedPoints)
     }, [])
-
-
-
-    useEffect(() => { createViewItemsDrag()}, [])
 
 
   
@@ -107,7 +227,7 @@ export default function ManipulandoArray (){
         return(
             <View style={styles.field}> 
 
-                <Text>{item.desc}</Text>
+                <Text>{item.hora}</Text>
             </View>
         )
     }
